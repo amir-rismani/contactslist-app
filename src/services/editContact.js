ffe7ceb0f8a03189ca94bb06384038f335ccd00b
Amir Rismaani <amir.rismaani@gmail.com>
2023-06-06 07:38:03 +0330

    Fix edit contact bug

--- src/services/editContact.js
+++ src/services/editContact.js
@@ -1,6 +1,5 @@
 import http from "./http";
 
 export default function editContact(contactId, data) {
-    console.log(contactId, data);
-    http.put(`/contacts/${contactId}`, data)
+    return http.put(`/contacts/${contactId}`, data)
 }
